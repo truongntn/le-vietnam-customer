7ee5b59656d2a8edfe585f1730da2b12ccd27b46
truongntn <truongntn@gmail.com>
2025-08-21 21:16:49 +0400

    update color

--- components/order-screen.tsx
+++ components/order-screen.tsx
@@ -485,13 +485,31 @@ export default function OrderScreen({
                   .map((item) => (
                     <div key={item.id} className="flex justify-between py-2">
                       <span>
-                        <p className="text-gray-600">
+                        <p
+                          className="text-gray-600"
+                          style={{
+                            color: "#F3B5FD",
+                            textShadow:
+                              "0 0 5px #F3B5FD, 0 0 10px #F3B5FD, 0 0 15px #F3B5FD",
+                            fontFamily: "monospace",
+                            display: "block",
+                          }}
+                        >
                           {" "}
                           {item.quantity} x {item.name}
                         </p>
                       </span>
                       <span>
-                        <p className="text-gray-600">
+                        <p
+                          className="text-gray-600"
+                          style={{
+                            color: "#F3B5FD",
+                            textShadow:
+                              "0 0 5px #F3B5FD, 0 0 10px #F3B5FD, 0 0 15px #F3B5FD",
+                            fontFamily: "monospace",
+                            display: "block",
+                          }}
+                        >
                           {" "}
                           ${(item.price * item.quantity).toFixed(2)}
                         </p>
