186ccf426c961dcb8d0ebf42a9a8fc7db12b1268
truongntn <truongntn@gmail.com>
2025-08-21 20:54:44 +0400

    update color

--- components/order-screen.tsx
+++ components/order-screen.tsx
@@ -306,7 +306,7 @@ export default function OrderScreen({
             <div className="mb-4">
               <label
                 htmlFor="name"
-                className="block text-sm  mb-1 font-bold text-gray-600"
+                className="block   mb-1 font-bold text-gray-600"
                 style={{
                   color: "#F3B5FD",
                   textShadow:
