de8d28f56b2f7135394cfc871cacf39365a22fff
truongntn <truongntn@gmail.com>
2025-08-23 14:45:40 +0400

    update color

--- components/order-screen.tsx
+++ components/order-screen.tsx
@@ -577,7 +577,7 @@ export default function OrderScreen({
           className=" rounded-lg shadow-lg  mb-6  bg-[#070923]"
           style={{ borderRadius: "0.0rem", border: "solid 1px #FFD2CC" }}
         >
-          <div className="p-4 bg-[#FFD2CC]">
+          <div className="p-4 bg-[#070923]">
             <h2
               className="text-xl font-bold"
               style={{
